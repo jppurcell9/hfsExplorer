9eb21ccc1cf82092b2fdad83d76846592ba07f0a
John Purcell <john.purcell@gmail.com>
2012-08-10 17:09:46 -0400

    remove some dead code

--- hfsBTree.h
+++ hfsBTree.h
@@ -15,32 +15,8 @@ public:
 	HfsBTree(HfsDriveInfo* drive, std::ifstream* backingStore, HFSPlusForkData* fork);
 	virtual ~HfsBTree();
 
-	//void dumpHeaderRec();
-
 protected:
 	NodeFactory* _nodeFactory;
-	// HfsDriveInfo* _driveInfo;
-	// std::ifstream* _backingStore;
-	// HFSPlusForkData* _fork;
-
-	// u_int16_t _nodeSize;
-	// u_int8_t* _headerNode;
-
-	// BTHeaderRec* _header;
-
-	// u_int64_t calcHeaderNodeOffset();
-	// u_int64_t calcOffsetFromNodeNumber(u_int32_t nodeNumber);
-
-	// void* search(BTreeKey* key, size_t dataLength);
-
-private:
-	// void init();
-	
-	// u_int16_t readNodeSize();
-	// std::streamsize readNode(u_int32_t nodeNumber, u_int8_t* buffer);
-
-	// void initNode(u_int8_t* buffer);
-	// void initHeaderNode(u_int8_t* buffer);
 };
 
 #endif
